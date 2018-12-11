eb980a4620dc29798e1399694979ee9a3ef5f1b9
Gabryel Ferreira <gabryel.ferreira@hotmail.com>
2018-12-11 00:20:41 -0200

    Adicionando funcao para listar alimentos por nome

--- src/Handler/Alimento.hs
+++ src/Handler/Alimento.hs
@@ -16,3 +16,9 @@ postSugerirAlimento = do
      alimento <- requireJsonBody :: Handler Tb_sugestao_alimento
      _ <- runDB $ insert alimento
      sendStatusJSON ok200 (object ["success" .= True])
+
+-- select alimentos por nome, com offset e limit para fazer infinite scroll
+getListAlimentoByNome :: Text -> Int -> Int -> Handler TypedContent
+getListAlimentoByNome nome offset limit = do 
+     alimentos <- runDB $ selectList [Filter Tb_alimentoNome (Left $  concat ["%", nome, "%"]) (BackendSpecificFilter "LIKE")] [LimitTo limit, OffsetBy offset]
+     sendStatusJSON ok200 (object ["success" .= True, "result" .= alimentos])
