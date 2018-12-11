a2bcac1887ff5c6ee255e9bd23b825c000f53afd
nickmaglowsch <nickmaglowsch3@gmail.com>
2018-12-11 00:13:51 -0200

    Adicionando funcao para parsear json

--- src/Handler/Usuario.hs
+++ src/Handler/Usuario.hs
@@ -19,3 +19,9 @@ requireJsonKey key jObject@(Object hashMap) = case lookup key hashMap of
                                     Nothing -> invalidArgs ["Couldn't find a value when looking up the key " <> key <> " in the object: " <> (pack (show jObject))]
                                     Just v -> return v
 requireJsonKey key invalid = invalidArgs ["When looking up the key " <> key <> ", expected an object but got a " ++ (pack (show invalid))]
+
+-- PARSEAR O JSON
+requireJsonParse :: (MonadHandler m, FromJSON a) => Value -> m a
+requireJsonParse v = case J.fromJSON v of
+  J.Error s -> invalidArgs [pack s]
+  J.Success a -> return a
