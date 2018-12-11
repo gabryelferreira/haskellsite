9391719869bc4a800825141c7e8c476f0fc66ddf
Larissa Rodrigues <larissalilian@gmail.com>
2018-12-11 00:04:43 -0200

    Adicionando handler de alimento

---
+++ src/Handler/Alimento.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveGeneric #-}
+module Handler.Alimento where
+
+import Import
+import Network.HTTP.Types.Status
+import Database.Persist.MySQL
+
