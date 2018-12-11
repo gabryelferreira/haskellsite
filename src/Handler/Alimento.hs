47b069e69c900a77940e7f0627b6fc0228fb768c
Larissa Rodrigues <larissalilian@gmail.com>
2018-12-11 00:05:22 -0200

    Adicionando funcao de sugestao de alimento

--- src/Handler/Alimento.hs
+++ src/Handler/Alimento.hs
@@ -10,3 +10,9 @@ import Import
 import Network.HTTP.Types.Status
 import Database.Persist.MySQL
 
+-- funcao para sugerir um alimento
+postSugerirAlimento :: Handler TypedContent
+postSugerirAlimento = do
+     alimento <- requireJsonBody :: Handler Tb_sugestao_alimento
+     _ <- runDB $ insert alimento
+     sendStatusJSON ok200 (object ["success" .= True])
